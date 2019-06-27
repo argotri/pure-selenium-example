e7a684a1480d595fdf421fc31092dc23d6d71288
Argo triwidodo <argo.triwidodo@yahoo.co.id>
2019-06-27 22:59:52 +0700

    Change chromedriver

--- src/test/java/id/web/gosoft/automation/blankmaven/AppTest.java
+++ src/test/java/id/web/gosoft/automation/blankmaven/AppTest.java
@@ -5,11 +5,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Every.everyItem;
 import static org.junit.Assert.assertTrue;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -26,7 +28,11 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        WebDriver webDriver = new ChromeDriver();
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        options.addArguments("window-size=1200x600");
+        WebDriver webDriver = new ChromeDriver(options);
         webDriver.get("https://www.google.co.id/");
         webDriver.findElement(By.name("q")).sendKeys("argo");
         try {
